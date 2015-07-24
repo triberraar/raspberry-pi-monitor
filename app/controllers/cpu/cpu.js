974972d541c4ef37a140c4fc82c0f60e2bf46390
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-07-24 11:06:50 +0200

    added loading

--- app/controllers/cpu/cpu.js
+++ app/controllers/cpu/cpu.js
@@ -3,16 +3,16 @@
 var spawn = require('child_process').spawn;
 var _ = require('lodash');
 var async = require('async');
-var process = require('./process');
+var loadAvg = require('./loadavg');
 
 var getCpuInfo = function(callback) {
     async.parallel(
-        [process.getProcesses
+        [loadAvg.getLoadAvg
             ], function (err, results) {
             if(err) {
                 console.error('error' + err);
             } else {
-                callback( {processes: results[0]});
+                callback( {loadAvg: results[0]});
             }
         }
     );
---
+++ app/controllers/cpu/loadavg.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs = require('fs'),
+    async = require('async');
+
+var readFile = function(callback) {
+    fs.readFile('/proc/loadavg', {encoding: 'binary'}, function(err, data) {
+       if(err) {
+           console.error('reading loadavg failed: ' + err);
+           callback(err);
+       } else {
+           callback(null, data);
+       }
+    });
+};
+
+var processLoadAvg = function(data, callback) {
+    var splittedData = data.split(/\s/);
+    callback(null, {'1min': splittedData[0], '5min': splittedData[1], '15min': splittedData[2]});
+};
+
+exports.getLoadAvg = function(callback) {
+    async.waterfall([
+        readFile,
+        processLoadAvg
+    ], function(err, result){
+        if(err) {
+            console.error('getLoadAvg failed: ', err);
+        } else {
+            callback(null, result);
+        }
+    })
+};
